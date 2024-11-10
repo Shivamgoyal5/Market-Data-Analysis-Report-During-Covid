9ccb4f47749bf99edc53cdf129787d2e300c3163
Shivamgoyal5 <shivamgoyal4545@gmail.com>
2024-11-10 14:18:41 +0530

    Update 1.py
    updated changes
    
    Co-Authored-By: gola0123 <187939933+gola0123@users.noreply.github.com>

--- pythoncode/1.py
+++ pythoncode/1.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 y=['Low Income','Lower Middle Income','Upper Middle Income','High Income']
 x=[4,5,3,1]
 
+
 plt.bar(y,x)
+plt.title('Analysis')
 plt.xlabel('Income Rate',fontweight='bold')
 plt.ylabel('Frequency',fontweight='bold')
 plt.grid(axis='y')
